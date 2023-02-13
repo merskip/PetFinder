8108b3f92650cc797740ed5ff9162cb2c8632390
Piotr Merski <merskip@gmail.com>
2023-02-13 20:01:50 +0100

    Added button find

--- PetFinder_Frame.lua
+++ PetFinder_Frame.lua
@@ -9,6 +9,13 @@ function PetFinder_FrameMixin:OnLoad()
     UIDropDownMenu_Initialize(self.petType3, AddPetTypesToDropdown)
 end
 
+function PetFinder_FrameMixin:FindOnClick()
+    local petType1 = UIDropDownMenu_GetSelectedValue(self.petType1)
+    local petType2 = UIDropDownMenu_GetSelectedValue(self.petType2)
+    local petType3 = UIDropDownMenu_GetSelectedValue(self.petType3)
+    print("Find pets for...", petType1, petType2, petType3)
+end
+
 function AddPetTypesToDropdown(dropdownMenu)
     local petTypes = GetPetTypesNames()
     for i, petType in ipairs(petTypes) do
